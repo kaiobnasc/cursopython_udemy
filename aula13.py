c1ef57becf9b981921e543453f62a867fc42e881
Kaio <kaionu2@gmail.com>
2024-02-29 19:35:18 -0300

    Estudando

---
+++ aula13.py
@@ -0,0 +1,12 @@
+nome = 'Kaio Nascimento'
+altura = 1.74
+peso = 64
+imc = peso/altura**2
+
+linha_1 = f'{nome} tem {altura:.2f} de altura'
+linha_2 = f'pesa {peso} quilos e seu imc é'
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(imc)
---
+++ aula2.py
@@ -0,0 +1,10 @@
+nome = input(str("Digite seu nome:"));
+idade = input(float("Digite sua idade: "));
+
+print('O nome é ', nome)
+
+if idade > 18:
+    print('Maior de idade')
+
+else:
+    print('Menor de idade');
