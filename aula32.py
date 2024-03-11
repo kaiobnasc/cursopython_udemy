69975cca986e09b82ebe3b7eb66e8c87fa2e600d
Kaio <kaionu2@gmail.com>
2024-03-11 20:21:48 -0300

    Estudando

--- aula32.py
+++ aula32.py
@@ -1,5 +1,5 @@
 # Exercícios
-entrada = input('Digite um número: ')
+'''entrada = input('Digite um número: ')
 
 if entrada.isdigit():
     entrada_int = float(entrada)
@@ -11,4 +11,33 @@ if entrada.isdigit():
 
     print(f'O número {entrada_int} é {par_impar_texto}')
 else:
-    print('Você não digitou um número inteiro.')
+    print('Você não digitou um número inteiro.)'''
+
+'''entrada = input('Digite a hora em números inteiros: ')
+
+try:
+    hora = int(entrada)
+
+    if hora >= 0 and hora <= 11:
+        print('Bom dia!')
+    elif hora >= 12 and hora <= 17:
+        print('Boa tarde')
+    elif hora >= 18 and hora <= 23:
+        print('Boa noite!')
+    else:
+        print('Não conheco essa hora')
+except:
+    print('Por favor digite apenas números inteiros.')'''
+
+nome = input('Digite seu nome: ')
+tamanho_nome = len(nome)
+
+if tamanho_nome > 1:
+    if tamanho_nome <= 4:
+        print('Seu nome é curto.')
+    elif tamanho_nome >= 5 and tamanho_nome <=6:
+        print('Seu nome é normal')
+    else:
+        print('Seu nome é muito grande.')
+else:
+    print('Digite mais de uma letra.')
---
+++ aula33.py
@@ -0,0 +1,6 @@
+string = 'kaio nascimento'
+outra_variavel = f'{string[:3]} ABC{string[4:]}'
+print(string)
+print(outra_variavel)
+print(string.capitalize()) #Coloca o K em maiúsculo
+print(string.zfill(100)) #Preenche com 0 (colocar o mouse em cima de 'zfill' para ver sua utilidade)
